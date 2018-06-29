d99cdd282c7859e852a82c2a1396507918f469e2
João Cabral <jv_cabral@outlook.com>
2018-06-29 15:16:46 -0300

    Programa copa

--- copa/copa.js
+++ copa/copa.js
@@ -13,18 +13,22 @@ $(function () {
             type: "GET",
             dataType: "json",
             success: function (result) {
+
                 $("body").html("");
-               // console.log(result[0].home_team.goals);
-               // console.log(result[0].away_team.goals);
+              
                $.each(result, function(index, value){
-                console.log(value.away_team.goals);
+                  
+
+                console.log(value);
                 time2 = value.away_team.country;
                 time1 = value.home_team.country;
                 
                 golstime1 = value.home_team.goals;
                 golstime2 = value.away_team.goals;
-              
-                $("body").append(" <div id='jogo1'> <span id='time1j1' class='certo'>"+time1+" &nbsp;</span> <span id='golstime1j1' class='certo'>"+golstime1+" &nbsp;</span> <span id='contra1' class='certo'>X &nbsp;</span> <span id='golstime2j1' class='certo'>"+golstime2+" &nbsp;</span> <span id='time2j1' class='certo'> "+time2+" &nbsp;</span> </div> <br />");
+              if(time2 != "To Be Determined" || time1 != "To Be Determined"){
+                $("body").append("<div id='jogo1'><div id='time1j1' class='certo'><span id='spanTime1'>"+time1+"</span> </div><div id='resultados'><div id='golstime1j1' class='certo'><span id='spanResultado1'>&nbsp; "+golstime1+" &nbsp;</span></div><div id='contra1' class='certo'><span id='contra'>X &nbsp;</span> </div><div id='golstime2j1' class='certo'><span id='spanResultado2'>"+golstime2+" &nbsp;</span></div></div><div id='time2j1' class='certo'><span id='spanTime2'>"+time2+"</span></div></div>");
+
+              }
                });
    
 
